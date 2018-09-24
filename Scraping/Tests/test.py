64b2beddc5111a4be7ea0b2ac3a0a33de49838a8
Andrew Krog <andrewdkrog@gmail.com>
2018-09-23 19:42:20 -0500

    web scrape app

--- Scraping/Tests/test.py
+++ Scraping/Tests/test.py
@@ -1,31 +1,29 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, jsonify, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-
+# create instance of Flask app
 app = Flask(__name__)
 
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-
-
+mongo = PyMongo(app)
 
 #  create route that renders index.html template
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
+    mars = scrape_mars.scrape()
     return render_template("index.html", mars=mars)
 
 
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
-    mars_data = scrape_mars.scrape()
-    mars.update(
-        {},
-        mars_data,
-        upsert=True
-    )
+    # mars_data = scrape_mars.scrape()
+    # mars.update(
+    #     {},
+    #     mars_data,
+    #     upsert=True
+    # )
     return redirect("http://localhost:5000/", code=302)
 
 
